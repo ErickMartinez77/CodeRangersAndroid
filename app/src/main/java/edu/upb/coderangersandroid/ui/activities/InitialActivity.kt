a89f1782003c916188bc772231fb1e8299517363
Erick Martinez <erickmartinezhuaylla123@gmail.com>
2021-10-03 22:06:45 -0400

    Initial screen done

---
+++ app/src/main/java/edu/upb/coderangersandroid/ui/activities/InitialActivity.kt
@@ -0,0 +1,22 @@
+package edu.upb.coderangersandroid.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import edu.upb.coderangersandroid.R
+
+class InitialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/edu/upb/coderangersandroid/ui/activities/LoginActivity.kt
@@ -0,0 +1,17 @@
+package edu.upb.coderangersandroid.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import edu.upb.coderangersandroid.R
+
+class LoginActivity: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+    }
+}
