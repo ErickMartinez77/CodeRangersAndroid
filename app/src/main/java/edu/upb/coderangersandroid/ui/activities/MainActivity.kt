6a33a0ee141d0793fc0067cf20322c3d613562d7
Maveto <maveto13@gmail.com>
2021-10-04 08:33:19 -0400

    Banch feature/PerfilActivity created

--- app/src/main/java/edu/upb/coderangersandroid/ui/activities/MainActivity.kt
+++ app/src/main/java/edu/upb/coderangersandroid/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package edu.upb.coderangersandroid
+package edu.upb.coderangersandroid.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.upb.coderangersandroid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/edu/upb/coderangersandroid/ui/activities/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package edu.upb.coderangersandroid.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.upb.coderangersandroid.R
+
+class ProfileActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
