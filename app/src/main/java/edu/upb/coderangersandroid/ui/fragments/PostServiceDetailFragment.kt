9ae7e73305f891eb6ee8b07ba55bd1e850647abc
Maveto <maveto13@gmail.com>
2021-10-18 07:55:56 -0400

    Fixed deatils from PostServiceDetail view

--- app/src/main/java/edu/upb/coderangersandroid/ui/fragments/PostServiceDetailFragment.kt
+++ app/src/main/java/edu/upb/coderangersandroid/ui/fragments/PostServiceDetailFragment.kt
@@ -9,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import edu.upb.coderangersandroid.R
 import edu.upb.coderangersandroid.model.Post
 import edu.upb.coderangersandroid.ui.adapters.FeedListAdapter
@@ -29,7 +30,10 @@ class PostServiceDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val serviceImage = view.findViewById<ImageView>(R.id.serviceImageDetail)
         val serviceOwnerNameText = view.findViewById<TextView>(R.id.serviceOwnerNameDetail)
-        val serviceDescriptionText = view.findViewById<TextView>(R.id.serviceNameDetail)
+        val serviceNameDetailText = view.findViewById<TextView>(R.id.serviceNameDetail)
+        val description = view.findViewById<TextView>(R.id.tvDescription)
+        val phoneNumberDetail = view.findViewById<TextView>(R.id.PhoneNumberDetail)
+        val serviceBackButton = view.findViewById<ImageView>(R.id.backButton)
 
         post = arguments?.getSerializable("post") as Post?
 
@@ -39,8 +43,13 @@ class PostServiceDetailFragment : Fragment() {
                 .transform(RoundedCorners(14))
                 .into(serviceImage)
             serviceOwnerNameText.text = it.ownerName
-            serviceDescriptionText.text = it.publisher
+            serviceNameDetailText.text = it.publisher
+            description.text = it.shortDescription
+            phoneNumberDetail.text = it.phone.toString()
         }
 
+        serviceBackButton.setOnClickListener{
+            parentFragmentManager.popBackStack()
+        }
     }
 }
