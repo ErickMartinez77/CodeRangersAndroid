da5a79fcc4dd47235261d69db702e982e33f56f8
Jorge Lopez <georgeilr99@gmail.com>
2021-10-22 08:51:47 -0300

    node red server addedd

---
+++ app/src/main/java/edu/upb/coderangersandroid/data/server-test.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+ 
+app.get('/posts', function (req, res) {
+  res.send([
+  {
+      "publisher" : "Plomero" ,
+      "shortDescription" : "Plomero con 5 años de experiencia en construcciones de edificios y casas",
+      "imageUrl" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfrCpMwWuUX7RQ9faN1IGzuRlnSFInU75oNg&usqp=CAU",
+      "ownerName" : "Mauricio Ayllon",
+      "phone" : 75409548
+  },
+  {
+      "publisher" : "Mecánico" ,
+      "shortDescription" : "Se arregla cualquier tipo de automóvil, tanto caja mecánica como automática ",
+      "imageUrl" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxuisYgMzkRXLWAHEGr0nv1JRa_Xc9XeSVJA&usqp=CAU",
+      "ownerName" : "Mauricio vejarano",
+      "phone" : 71446548
+  },
+  {
+      "publisher" : "Pintor" ,
+      "shortDescription" : "Se pinta dormitorios, cocinas, hasta Fachadas externas",
+      "imageUrl" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf0_jpp2Q_YqBzpGYmfC7NqGTFEs3qni2OKg&usqp=CAU",
+      "ownerName" : "Erick Martinez",
+      "phone" : 73098346
+  },
+  {
+      "publisher" : "Servicio Técnico de Celulares" ,
+      "shortDescription" : "Se arreglan celulares android y Ios",
+      "imageUrl" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9UPPb129gDteN5FtcoXbUWWXjByYVnhAKsg&usqp=CAU",
+      "ownerName" : "Jorge Lopez",
+      "phone" : 72900043
+  },
+  {
+      "publisher" : "Jardinero" ,
+      "shortDescription" : "Superviso las plantas de su jardin así como los espacios verdes",
+      "imageUrl" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw0nk5mw8CWiieHqekvsl_bttQhDcVWy6goQ&usqp=CAU",
+      "ownerName" : "Kevin Pomier",
+      "phone" : 74637899
+  }
+])
+})
+
+app.listen(3000, "10.100.1.16")
